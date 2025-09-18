ae2df94d8af3e3473f2254c6eda0521d753e84aa
pavan wishva <pavanwish2002@gmail.com>
2025-09-18 11:56:25 +0530

    erase repository data

--- backend/src/main/java/com/example/librarybackend/reservation/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.librarybackend.reservation;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ReservationRepository extends MongoRepository<Reservation, String> {
-}
-
-
-
