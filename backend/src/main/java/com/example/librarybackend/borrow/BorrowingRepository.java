6154a8ed5fbd008256f58460b1c0b1d268dc5284
pavan wishva <pavanwish2002@gmail.com>
2025-09-18 11:58:34 +0530

    correctly change repository document

--- backend/src/main/java/com/example/librarybackend/borrow/BorrowingRepository.java
+++ backend/src/main/java/com/example/librarybackend/borrow/BorrowingRepository.java
@@ -1,1 +1,10 @@
+package com.example.librarybackend.borrow;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BorrowingRepository extends MongoRepository<Borrowing, String> {
+    boolean existsByBorrowingNumber(String borrowingNumber);
+}
+
+
 
