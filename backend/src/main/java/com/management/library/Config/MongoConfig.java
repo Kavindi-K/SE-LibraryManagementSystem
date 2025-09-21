2c266739178392078da131a856f3b30ffc3535ba
pavan wishva <pavanwish2002@gmail.com>
2025-09-21 20:59:50 +0530

    configure mongoDb for my Crudoperation

--- backend/src/main/java/com/management/library/Config/MongoConfig.java
+++ backend/src/main/java/com/management/library/Config/MongoConfig.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 
 @Configuration
 @EnableMongoAuditing
-@EnableMongoRepositories(basePackages = "com.management.library.UserManagement.Repository")
+@EnableMongoRepositories(basePackages = {"com.management.library.UserManagement.Repository", "com.management.library.BorrowingReservation.repository"})
 public class MongoConfig {
     // MongoDB's configuration is handled by application.properties
     // This class enables auditing for @CreatedDate and @LastModifiedDate
