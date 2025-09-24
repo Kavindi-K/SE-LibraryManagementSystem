1cfc9fab4667955d3a0a072d5de3e4ac1a32355c
pavan wishva <pavanwish2002@gmail.com>
2025-09-24 19:10:15 +0530

    edit borrowing repo

--- backend/src/main/java/com/management/library/BorrowingReservation/repository/BorrowingRepository.java
+++ backend/src/main/java/com/management/library/BorrowingReservation/repository/BorrowingRepository.java
@@ -6,6 +6,8 @@ import com.management.library.BorrowingReservation.entity.Borrowing;
 
 public interface BorrowingRepository extends MongoRepository<Borrowing, String> {
     boolean existsByBorrowingNumber(String borrowingNumber);
+    java.util.List<Borrowing> findByMemberId(String memberId);
+    boolean existsByMemberIdAndBookIdAndStatus(String memberId, String bookId, String status);
 }
 
 
